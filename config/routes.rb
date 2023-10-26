0ee41364b31c8b8a587ec5d9475dc81ef97f8281
Seth Rojas <patrojas.martinez@gmail.com>
2023-10-26 17:52:29 -0300

    bootstrap.jt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :admins controllers: { registrations: 'registrations' }
+  
+  get 'home/index'
   get 'home/client'
   get 'home/user'
+  get 'home/admin'
   resources :clients
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
